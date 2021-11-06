39ad2d479b2cccc83a13b1199c7f1b82558fcac3
nirpendra83 <93668666+nirpendra83@users.noreply.github.com>
2021-11-06 20:49:15 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.84.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+}
